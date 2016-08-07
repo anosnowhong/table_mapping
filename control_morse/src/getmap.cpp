d3aa03fe73115515413a12741ed2b6ee9e7caba7
Hongru <anosnowhong@gmail.com>
2016-08-07 16:17:42 +0100

    fix 2d map wrong order (col & row)

--- control_morse/src/getmap.cpp
+++ control_morse/src/getmap.cpp
@@ -3,38 +3,62 @@
 #include <nav_msgs/OccupancyGrid.h>
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
+#include <fstream>
 
 void map_analyzer(nav_msgs::OccupancyGrid &map)
 {
+    int rows = map.info.width;
+    int cols = map.info.height;
+
     //create and init map data matrix
     std::vector<std::vector<int> > matrix_map;
-    matrix_map.resize(map.info.width);
-    for(int i=0;i<map.info.width;i++)
-        matrix_map[i].resize(map.info.height);
+    matrix_map.resize(cols);
+    for(int i=0;i<cols;i++)
+        matrix_map[i].resize(rows);
 
     //convert map data to matrix
     int ros_map_cell=0;
-    for(int i=0;i<map.info.width;i++) {
-        for (int j = 0; j < map.info.height; j++) {
+    for(int i=0;i<cols;i++) {
+        for (int j = 0; j < rows; j++) {
             matrix_map[i][j] = map.data[ros_map_cell];
             ros_map_cell++;
         }
     }
 
-    cv::Mat cv_map(map.info.width, map.info.height, CV_8SC1);
+    cv::Mat cv_map(cols, rows, CV_8UC1);
+    //cv::Mat origin_map(map.info.width, map.info.height, CV_64F);
 
-    for(int i=0;i<map.info.width;i++)
+    for(int i=0;i<cols;i++)
     {
-        for(int j=0;j<map.info.height;j++) {
+        for(int j=0;j<rows;j++) {
             if(matrix_map[i][j] == 0)
             {
-                cv_map.at<char>(i,j) = 0;
+                cv_map.at<char>(i,j) = 255;
+                //origin_map.at<double>(i,j) = matrix_map[i][j];
             }
             else
-                cv_map.at<char>(i,j) = 1;
+            {
+                cv_map.at<char>(i,j) = 0;
+                //origin_map.at<double>(i,j) = matrix_map[i][j];
+            }
         }
     }
     std::cout<<cv_map<<std::endl;
+
+    /*
+    std::ofstream matrix, cvmat;
+    matrix.open("matrix.txt");
+    matrix<<origin_map;
+    matrix.close();
+    cvmat.open("cvmat.txt");
+    cvmat<<cv_map;
+    cvmat.close();
+     */
+
+    cv::imwrite("cvimg.pgm",cv_map);
+    cv::namedWindow( "ros occupancy map", cv::WINDOW_NORMAL);
+    cv::imshow("ros occupancy map", cv_map);
+    cv::waitKey(0);
 
 }
 
