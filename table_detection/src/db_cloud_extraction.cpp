3135d2f30b5b1b691a5c88c38ba5ec1dd329af4e
Hongru <anosnowhong@gmail.com>
2016-08-17 20:56:08 +0100

    whole table extractor service

--- table_detection/src/db_cloud_extraction.cpp
+++ table_detection/src/db_cloud_extraction.cpp
@@ -1,4 +1,3 @@
-
 #include <mongodb_store/message_store.h>
 #include <geometry_msgs/Pose.h>
 #include <geometry_msgs/TransformStamped.h>
@@ -6,6 +5,11 @@
 #include <primitive_extraction/ExtractPrimitives.h>
 #include <pcl_ros/point_cloud.h>
 #include <pcl/io/pcd_io.h>
+#include <table_detection/db_extract.h>
+#include <table_detection/db_extract_whole_table.h>
+#include <primitives_to_tables/PrimitivesToTables.h>
+#include <strands_perception_msgs/Table.h>
+
 
 /* #A backend node that does the extraction work with MongoDB.
  * Load point cloud data from global_cloud collection.
@@ -13,64 +17,205 @@
  * use table parameter to filer out table plane
  * store to table_plane collection
  * */
-typedef boost::shared_ptr<geometry_msgs::TransformStamped>  TransformStampedPtr;
-typedef boost::shared_ptr<sensor_msgs::PointCloud2> PointCloud2Ptr;
 
-#define Debug true
+ros::ServiceClient primitive_client;
+ros::NodeHandlePtr nh;
 
-int main(int argc, char** argv)
+double min_height;
+double max_height;
+double max_angle;
+double min_side_ratio;
+double min_area;
+
+int checked_num=0;
+
+#define Debug false
+
+double compute_plane_area(std::vector<geometry_msgs::Point>& hull)
 {
-/*
-    ros::init(argc, argv, "db_cloud_registration");
-    ros::NodeHandle nh;
+    double area = 0.0;
+    // assume that it's almost parallell to the ground
+    Eigen::Vector2d p0(hull[0].x, hull[0].y);
+    for (size_t i = 1; i < hull.size()-1; ++i) {
+        Eigen::Vector2d p1(hull[i].x, hull[i].y);
+        Eigen::Vector2d p2(hull[i+1].x, hull[i+1].y);
+        // calculate area by Heron's formula
+        // this can be done by det as in table_tracking
+        double a = (p1-p0).norm();
+        double b = (p2-p0).norm();
+        double c = (p2-p1).norm();
+        double s = 0.5*(a + b + c);
+        area += sqrt(s*(s-a)*(s-b)*(s-c));
+    }
+    return area;
+}
 
-    ros::ServiceClient client=nh.serviceClient<primitive_extraction::ExtractPrimitives>("extract_primitives");
+void table_filter(primitive_extraction::PrimitiveArray &input,primitive_extraction::PrimitiveArray &output)
+{
+    output.camera_frame = input.camera_frame;
+    size_t n = input.primitives.size();
+
+    for (size_t i = 0; i < n; ++i) {
+        primitive_extraction::Primitive p = input.primitives[i];
+
+        // check normal
+        double alpha = acos(fabs(p.params[4]));
+        if (alpha > max_angle) {
+            //ROS_INFO("Stopped because of angle: %f", alpha);
+            continue;
+        }
+
+        double camera_height = 0.0;
+        // check height
+        double height = camera_height + p.pose.position.z;
+        if (height < min_height || height > max_height) {
+            //ROS_INFO("Stopped because of height: %f", height);
+            continue;
+        }
+
+        // check size
+        double area = compute_plane_area(p.points);
+        if (area < min_area) {
+            //ROS_INFO("Stopped because of area: %f", area);
+            continue;
+        }
+
+        // check shape
+        double minside, maxside;
+        if (p.params[0] > p.params[1]) {
+            minside = p.params[1];
+            maxside = p.params[0];
+        }
+        else {
+            minside = p.params[0];
+            maxside = p.params[1];
+        }
+
+        double ratio = minside/maxside;
+        if (ratio < min_side_ratio) {
+            //ROS_INFO("Stopped because of ratio: %f", ratio);
+            continue;
+        }
+
+        output.primitives.push_back(p);
+        //indices.push_back(i);
+    }
+}
+
+bool extract(table_detection::db_extract::Request &req, table_detection::db_extract::Response &res)
+{
     //query point cloud and tf
-    mongodb_store::MessageStoreProxy messageStore(nh,"global_clouds");
+    mongodb_store::MessageStoreProxy global_clouds(*nh,"global_clouds");
     //store point cloud that been transformed
-    mongodb_store::MessageStoreProxy cloud_store(nh,"table_planes");
+    mongodb_store::MessageStoreProxy table_store(*nh,"table_planes");
     std::vector< boost::shared_ptr<sensor_msgs::PointCloud2> > result_pc2;
 
     //search all point clouds
-    messageStore.query<sensor_msgs::PointCloud2>(result_pc2);
+    global_clouds.query<sensor_msgs::PointCloud2>(result_pc2);
 
     std::cout<<"pc2: "<<result_pc2.size()<<std::endl;
-    //std::cout<<"tf: "<<result_tf.size()<<std::endl;
 
-    pcl::PointCloud<pcl::PointXYZ>::Ptr db_global_cloud(new pcl::PointCloud<pcl::PointXYZ>());
-    pcl::PointCloud<pcl::PointXYZ>::Ptr db_store(new pcl::PointCloud<pcl::PointXYZ>());
-    sensor_msgs::PointCloud2 db_store_msg;
+    //pcl::PointCloud<pcl::PointXYZ>::Ptr db_global_cloud(new pcl::PointCloud<pcl::PointXYZ>());
 
-    for(int i=0;i<result_pc2.size();i++)
+    for(;checked_num<result_pc2.size();checked_num++)
     {
         //reset
-        db_global_cloud.reset(new pcl::PointCloud<pcl::PointXYZ>);
-        db_store.reset(new pcl::PointCloud<pcl::PointXYZ>);
+        //db_global_cloud.reset(new pcl::PointCloud<pcl::PointXYZ>);
 
         //load point cloud data to pcl::PointCloud2
-        pcl::fromROSMsg(*result_pc2[i],*db_global_cloud);
-        //call the extraction service
+        //pcl::fromROSMsg(*result_pc2[i],*db_global_cloud);
 
+        //call the extraction service,get primitives planes
+        primitive_extraction::ExtractPrimitives req;
+        req.request.pointcloud=*result_pc2[checked_num];
+        primitive_client.call(req);
 
-        if(Debug){
-            std::string name="/home/parallels/debug/.pcd";
-            std::stringstream ss;
-            ss<< i;
-            std::string str = ss.str();
-            name.insert(name.length()-4, str);
-            std::cout<<name<<std::endl;
-            pcl::io::savePCDFileASCII(name, *db_store);
+        //filter out possible table planes
+        primitive_extraction::PrimitiveArray to_table;
+        table_filter(req.response.primitives,to_table);
+
+        //fill the table msg
+        std::vector<strands_perception_msgs::Table> tables;
+        tables.resize(to_table.primitives.size());
+        for (size_t i = 0; i < to_table.primitives.size(); ++i) {
+            strands_perception_msgs::Table table;
+            primitive_extraction::Primitive primitive = to_table.primitives[i];
+            table.header.frame_id = to_table.camera_frame;
+            table.header.stamp = result_pc2[checked_num]->header.stamp;
+            table.pose.pose = primitive.pose;
+            table.tabletop.points.resize(primitive.points.size());
+            for (size_t j = 0; j < primitive.points.size(); ++j) {
+                table.tabletop.points[j].x = primitive.points[j].x;
+                table.tabletop.points[j].y = primitive.points[j].y;
+                table.tabletop.points[j].z = primitive.points[j].z;
+            }
+            tables[i] = table;
+            //insert it to mongodb
+            table_store.insert(table);
         }
 
-        //convert to msg and store it
-        pcl::toROSMsg(*db_store,db_store_msg);
-
-        //keep same header but different frame_id
-        db_store_msg.header=result_pc2[i]->header;
-        db_store_msg.header.frame_id = "/map";
-        cloud_store.insert(db_store_msg);
-
+        //store the index of point cloud as well
+        if(to_table.primitives.size()>0)
+            res.cloud_has_plane.push_back(checked_num);
     }
-    */
-    return 0;
+
+    if(Debug)
+    {
+        for(int i=0;i<res.cloud_has_plane.size();i++)
+            std::cout<<res.cloud_has_plane[i]<<std::endl;
+    }
+    return true;
+}
+
+bool extract_whole_table(table_detection::db_extract_whole_table::Request &req, table_detection::db_extract_whole_table::Response &res)
+{
+    mongodb_store::MessageStoreProxy whole_tables(*nh,"whole_tables");
+
+    //call the extraction service,get primitives planes
+    primitive_extraction::ExtractPrimitives primitives_req;
+    primitives_req.request.pointcloud=req.cloud;
+    primitive_client.call(primitives_req);
+
+    //filter out possible table planes
+    primitive_extraction::PrimitiveArray to_table;
+    table_filter(primitives_req.response.primitives,to_table);
+
+     std::vector<strands_perception_msgs::Table> tables;
+        tables.resize(to_table.primitives.size());
+        for (size_t i = 0; i < to_table.primitives.size(); ++i) {
+            strands_perception_msgs::Table table;
+            primitive_extraction::Primitive primitive = to_table.primitives[i];
+            table.header.frame_id = to_table.camera_frame;
+            //table.header.stamp = result_pc2[checked_num]->header.stamp;
+            table.pose.pose = primitive.pose;
+            table.tabletop.points.resize(primitive.points.size());
+            for (size_t j = 0; j < primitive.points.size(); ++j) {
+                table.tabletop.points[j].x = primitive.points[j].x;
+                table.tabletop.points[j].y = primitive.points[j].y;
+                table.tabletop.points[j].z = primitive.points[j].z;
+            }
+            tables[i] = table;
+            //insert it to mongodb
+            whole_tables.insert(table);
+        }
+    return true;
+}
+
+int main(int argc, char** argv)
+{
+    ros::init(argc, argv, "db_cloud_extraction");
+    nh.reset(new ros::NodeHandle);
+    ros::NodeHandle pn("~");
+
+    pn.param<double>("min_height", min_height, 0.55);
+    pn.param<double>("max_height", max_height, 1.1);
+    pn.param<double>("max_angle", max_angle, 0.314);
+    pn.param<double>("min_side_ration", min_side_ratio, 0.25);
+    pn.param<double>("min_area", min_area, 0.3);
+
+    primitive_client=nh->serviceClient<primitive_extraction::ExtractPrimitives>("extract_primitives");
+    ros::ServiceServer ext_srv = nh->advertiseService("db_extract", extract);
+    ros::ServiceServer ext_srv2 = nh->advertiseService("db_extract_whole_table", extract_whole_table);
+
+    ros::spin();
 }
