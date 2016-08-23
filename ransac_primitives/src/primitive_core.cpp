56ad58cf069d241ba6ef0de68e92a8bfa489c79b
Hongru <anosnowhong@gmail.com>
2016-08-23 11:45:36 +0100

    remove unnecessary denpencies

--- ransac_primitives/src/primitive_core.cpp
+++
@@ -1,8 +0,0 @@
-#include "ransac_primitives/primitive_core.h"
-#include <pcl/point_types.h>
-
-template class primitive_extractor<pcl::PointXYZRGB>;
-template class primitive_extractor<pcl::PointXYZ>;
-
-template class primitive_visualizer<pcl::PointXYZRGB>;
-template class primitive_visualizer<pcl::PointXYZ>;
